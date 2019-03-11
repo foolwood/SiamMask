ccb333b1b7eb40b11b76ea4c3ee47945ff98e398
103yiran <1039105206@qq.com>
2019-03-11 15:24:32 +0800

    Simplify the computation
    sz_wh(target_sz_in_crop)=p.exemplar_size
    target_sz_in_crop[0] / target_sz_in_crop[1]=target_sz[0] / target_sz[1]

--- tools/test.py
+++ tools/test.py
@@ -216,15 +216,9 @@ def siamese_track(state, im, mask_enable=False, refine_enable=False):
         sz2 = (w + pad) * (h + pad)
         return np.sqrt(sz2)
 
-    def sz_wh(wh):
-        pad = (wh[0] + wh[1]) * 0.5
-        sz2 = (wh[0] + pad) * (wh[1] + pad)
-        return np.sqrt(sz2)
-
     # size penalty
-    target_sz_in_crop = target_sz*scale_x
-    s_c = change(sz(delta[2, :], delta[3, :]) / (sz_wh(target_sz_in_crop)))  # scale penalty
-    r_c = change((target_sz_in_crop[0] / target_sz_in_crop[1]) / (delta[2, :] / delta[3, :]))  # ratio penalty
+    s_c = change(sz(delta[2, :], delta[3, :]) / p.exemplar_size)  # scale penalty
+    r_c = change((target_sz[0] / target_sz[1]) / (delta[2, :] / delta[3, :]))  # ratio penalty
 
     penalty = np.exp(-(r_c * s_c - 1) * p.penalty_k)
     pscore = penalty * score
